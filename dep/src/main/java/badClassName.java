3a6c0c0fe6d894619ffb22be5bbb5ebc919e2a86
argus-hongjie <hongjie.zhang@rse.argus.int>
2017-02-16 16:02:48 +0100

    asdf

---
+++ dep/src/main/java/badClassName.java
@@ -0,0 +1,19 @@
+
+
+public class badClassName {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public void BadMethodName(){
+		System.out.println("asdfsa");
+		return;
+	}
+
+	public void BadMethodName2(){
+		System.out.println("asdfsa");
+		return;
+	}
+}
